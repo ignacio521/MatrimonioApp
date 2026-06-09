299bd5440da0304c13920972b5d2ae5967fa27e6
exibaor <exibaor@bci.cl>
2026-06-09 12:50:21 -0400

    Foto Luki al confirmar cancion

--- src/app/pages/canciones-sugeridas/canciones-sugeridas.component.ts
+++ src/app/pages/canciones-sugeridas/canciones-sugeridas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeddingDataService, MusicSuggestion } from '../../services/wedding-data.service';
 
 @Component({
@@ -6,20 +6,29 @@ import { WeddingDataService, MusicSuggestion } from '../../services/wedding-data
   templateUrl: './canciones-sugeridas.component.html',
   styleUrls: ['./canciones-sugeridas.component.css']
 })
-export class CancionesSugeridasComponent implements OnInit {
+export class CancionesSugeridasComponent implements OnInit, OnDestroy {
   readonly deleteConfirmImagePath = '/assets/img/FotosParaCards/PHOTO-2026-06-02-11-14-21 (1).jpg';
+  readonly songConfirmImagePath = '/assets/img/Music/WhatsApp Image 2026-06-09 at 12.47.05 PM.jpeg';
   private pendingDeleteId: string | null = null;
+  private feedbackTimer: ReturnType<typeof setTimeout> | null = null;
 
   musicTitle = '';
   musicArtist = '';
   musicSuggestions: MusicSuggestion[] = [];
   musicFormError = '';
   showDeleteConfirm = false;
+  showSongSubmitFeedback = false;
 
   constructor(private readonly weddingDataService: WeddingDataService) {}
 
   ngOnInit(): void {
     this.loadMusicSuggestions();
+  }
+
+  ngOnDestroy(): void {
+    if (this.feedbackTimer) {
+      clearTimeout(this.feedbackTimer);
+    }
   }
 
   addMusicSuggestion(): void {
@@ -50,6 +59,7 @@ export class CancionesSugeridasComponent implements OnInit {
         this.musicSuggestions = this.musicSuggestions.filter(item => item.id !== suggestion.id);
       }
     });
+    this.launchSongSubmitFeedback();
     this.resetMusicForm();
   }
 
@@ -100,6 +110,23 @@ export class CancionesSugeridasComponent implements OnInit {
   private resetMusicForm(): void {
     this.musicTitle = '';
     this.musicArtist = '';
+  }
+
+  private launchSongSubmitFeedback(): void {
+    this.showSongSubmitFeedback = false;
+
+    setTimeout(() => {
+      this.showSongSubmitFeedback = true;
+    }, 0);
+
+    if (this.feedbackTimer) {
+      clearTimeout(this.feedbackTimer);
+    }
+
+    this.feedbackTimer = setTimeout(() => {
+      this.showSongSubmitFeedback = false;
+      this.feedbackTimer = null;
+    }, 3000);
   }
 
   private hashCode(value: string): number {
